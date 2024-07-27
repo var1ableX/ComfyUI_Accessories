56e932e99709f6babb7b580b54169402fdc749db
var1ableX <raj.kotecha@gmail.com>
2024-07-26 20:59:47 -0400

    initial commit

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from .maskToCrop import GetMaskDimensions
+
+# Node class mappings
+NODE_CLASS_MAPPINGS = {
+    "GetMaskDimensions": GetMaskDimensions,
+}
+
+# Node display name mappings
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "GetMaskDimensions": "Get Mask Dimensions",
+}
